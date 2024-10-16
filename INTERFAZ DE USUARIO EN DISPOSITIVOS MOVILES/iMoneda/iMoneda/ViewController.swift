ef18d72a269fed0040817ace8135bb5c1069c79d
Juangm14 <jgm179@gcloud.ua.es>
2024-10-16 17:11:29 +0200

    Práctica 1 iOS terminada.

--- INTERFAZ DE USUARIO EN DISPOSITIVOS MOVILES/iMoneda/iMoneda/ViewController.swift
+++ INTERFAZ DE USUARIO EN DISPOSITIVOS MOVILES/iMoneda/iMoneda/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let miMoneda = Moneda()
 
+    @IBOutlet weak var imageMonedaView: UIImageView!
+    @IBOutlet weak var respuestaLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func lanzarMoneda(_ sender: Any) {
+        
+        
+        let respuesta = miMoneda.lanzarMoneda()
+        var ladoMoneda = ""
+        
+        if respuesta == Tirada.cara{
+            ladoMoneda = "cara"
+        }
+        else{
+            ladoMoneda = "cruz"
+        }
+        
+        respuestaLabel.text = "¡Ha salido " + ladoMoneda + "!"
+        let imagenCara = UIImage(named:ladoMoneda)
+        self.imageMonedaView.image = imagenCara
+    }
+                
 }
 
