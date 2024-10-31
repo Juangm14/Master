4b56ca5fcc8b37a05f31bb90e7c291f4beaeec84
Juangm14 <jgm179@gcloud.ua.es>
2024-10-31 17:32:51 +0100

    Pioneras. Hecho hasta el ejercicio 2.

---
+++ INTERFAZ DE USUARIO EN DISPOSITIVOS MOVILES/Pioneras/Pioneras/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Pioneras
+//
+//  Created by Master Moviles on 31/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func retornoDeSecundaria(segue: UIStoryboardSegue) {
+        
+    }
+    
+}
