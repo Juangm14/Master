715cbb8ec6e1a2f81b039fb2a2db3dcd1fa2b33b
Juan García Martínez <jgm179@alu.ua.es>
2024-11-21 19:54:06 +0100

    Incluyendo deshabilitar envio al estar desconectado y layout chat

--- TECNOLOGIAS PARA EL DESARROLLO DE APLICACIONES MOVILES/app/src/main/java/es/ua/eps/chatbot/MainActivity.kt
+++ TECNOLOGIAS PARA EL DESARROLLO DE APLICACIONES MOVILES/app/src/main/java/es/ua/eps/chatbot/MainActivity.kt
@@ -1,6 +1,7 @@
 package es.ua.eps.chatbot
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Base64
 import android.view.Gravity
@@ -16,6 +17,10 @@ import java.io.*
 import java.net.Socket
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
+import java.text.SimpleDateFormat
+import java.util.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,18 +40,28 @@ class MainActivity : AppCompatActivity() {
         binding.targetAddress.setText(serverIP)
         binding.port.setText(serverPort.toString())
 
+        // Deshabilitar el botón de envío y el campo de texto
+        binding.sendButton.isEnabled = false
+        binding.messageInput.isEnabled = false
+
         binding.newSocketButton.setOnClickListener {
             val targetAddress = binding.targetAddress.text.toString()
             aliasClient = binding.alias.text.toString()
 
             val port = binding.port.text.toString()
+
             if (targetAddress.isNotBlank() && port.isNotBlank()) {
                 serverIP = targetAddress
                 serverPort = port.toIntOrNull() ?: 0
                 if (serverPort > 0 && esDireccionIPv4Valida(targetAddress)) {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        closeConnection()
-                        connectToServer()
+
+                    if (aliasClient.isEmpty()) {
+                        showToast("Por favor, ingresa un alias válido.")
+                    }else {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            closeConnection()
+                            connectToServer()
+                        }
                     }
                 } else {
                     showToast("Dirección o puerto inválido.")
@@ -74,6 +89,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Encripta un texto usando AES 16 Bytes y lo convertimos a BA64.
     fun cifrarTexto(texto: String, claveSecreta: String): String {
         try {
             // Convierte la clave secreta en un formato adecuado
@@ -94,6 +110,7 @@ class MainActivity : AppCompatActivity() {
         return ""
     }
 
+    // Decodifica el BA64 primero y luego desencripta un texto cifrado en base64 usando AES 16 Bytes
     fun descifrarTexto(textoCifrado: String, claveSecreta: String): String {
         try {
             // Convierte la clave secreta en un formato adecuado
@@ -117,6 +134,7 @@ class MainActivity : AppCompatActivity() {
         return ""
     }
 
+    //En caso de tener un socket activo, lo cierra y crea otro nuevo. Se muestra un mensaje de que ha establecido conexión o error en caso contrario.
     private fun connectToServer() {
         try {
             socket?.close()
@@ -127,6 +145,10 @@ class MainActivity : AppCompatActivity() {
             runOnUiThread {
                 binding.connectionStatus.text = "Conectado a $serverIP:$serverPort"
                 showToast("Conexión establecida")
+
+                // Habilitar el botón de envío y el campo de texto
+                binding.sendButton.isEnabled = true
+                binding.messageInput.isEnabled = true
             }
 
             CoroutineScope(Dispatchers.IO).launch {
@@ -136,18 +158,29 @@ class MainActivity : AppCompatActivity() {
             runOnUiThread {
                 binding.connectionStatus.text = "Conexión fallida"
                 showToast("Error al conectar: ${e.message}")
+                // Deshabilitar el botón de envío y el campo de texto
+                binding.sendButton.isEnabled = false
+                binding.messageInput.isEnabled = false
             }
         }
     }
 
+    // Escucha los mensajes entrantes del servidor y llama a las funciones necesarias para descifrar (con la misma clave secreta) y
+    // escribir los mensajes extrayendo el alias mediante un split de ":"
     private fun listenForMessages() {
         try {
             var message: String?
             while (input?.readLine().also { message = it } != null) {
-                // La IP ya está incluida en el mensaje por el servidor
                 runOnUiThread {
-                    var mensaje = descifrarTexto(message!!, claveSecreta = "clavesecreta1234")
-                    binding.messageContainer.addView(createMessageTextView(mensaje!!, isMine = false))
+                    val mensajeDescifrado = descifrarTexto(message!!, claveSecreta = "clavesecreta1234")
+                    val partes = mensajeDescifrado.split(":", limit = 2)
+                    if (partes.size == 2) {
+                        val alias = partes[0] // Extraer alias
+                        val contenido = partes[1] // Extraer mensaje
+                        binding.messageContainer.addView(createMessageLayout(contenido, isMine = false, alias))
+                    } else {
+                        binding.messageContainer.addView(createMessageLayout(mensajeDescifrado, isMine = false, "Desconocido"))
+                    }
                 }
             }
         } catch (e: IOException) {
@@ -157,13 +190,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Se envia un mensaje al servidor. Con el alias del cliente y su mensaje cifrado mediante AESS 16bytes (Los Bytes lo define la clave secreta)
     fun sendTextMessage(message: String) {
-        val mensajeCifrado = cifrarTexto("$aliasClient: $message", "clavesecreta1234") //Clave secreta
+        val mensajeCifrado = cifrarTexto("$aliasClient:$message", "clavesecreta1234") //Clave secreta
         try {
             output?.println(mensajeCifrado)  // Enviar el mensaje cifrado
             output?.flush()
             runOnUiThread {
-                binding.messageContainer.addView(createMessageTextView("Tú: $message", isMine = true))
+                binding.messageContainer.addView(createMessageLayout("$message", isMine = true, aliasClient))
                 binding.messageInput.text.clear()
             }
         } catch (e: IOException) {
@@ -173,12 +207,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Cerramos la conexión con el servidor(cierra el socket)
     private fun closeConnection() {
         try {
             socket?.close()
             socket = null
             runOnUiThread {
                 binding.connectionStatus.text = "Desconectado"
+
+                // Deshabilitar el botón de envío y el campo de texto
+                binding.sendButton.isEnabled = false
+                binding.messageInput.isEnabled = false
+
             }
         } catch (e: IOException) {
             runOnUiThread {
@@ -187,49 +227,73 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun createMessageTextView(message: String, isMine: Boolean): TextView {
-        val textView = TextView(this)
-        val params = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
+    //Obtenemos la hora central española.
+    fun obtenerHora(): String {
+        val zonaEspaña = TimeZone.getTimeZone("Europe/Madrid")
+        val calendario = Calendar.getInstance(zonaEspaña)
+        val formato = SimpleDateFormat("HH:mm", Locale.getDefault()) // Cambia el formato si es necesario
+        formato.timeZone = zonaEspaña
+        return formato.format(calendario.time)
+    }
 
-        params.gravity = if (isMine) Gravity.END else Gravity.START
+    // Crea un layout para mostrar el mensaje con su alias, contenido y hora. Empleamos drawables para los colores y la forma del mensaje y sea una forma de bocadillo.
+    private fun createMessageLayout(message: String, isMine: Boolean, alias: String): LinearLayout {
 
-        params.topMargin = 16
-        params.bottomMargin = 16
-        if (isMine) {
-            params.leftMargin = 150
-        }else {
-            params.rightMargin = 150
+        val messageLayout = LinearLayout(this).apply {
+            orientation = LinearLayout.VERTICAL
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.gravity = if (isMine) Gravity.END else Gravity.START
+            params.topMargin = 16
+            params.bottomMargin = 16
+            if (isMine) {
+                params.leftMargin = 150
+            } else {
+                params.rightMargin = 150
+            }
+            layoutParams = params
+            setPadding(16, 16, 16, 16)
+            setBackgroundResource(if (isMine) R.drawable.bocadillo_salida else R.drawable.bocadillo_entrada)
         }
 
-        textView.layoutParams = params
+        val aliasText = TextView(this).apply {
+            text = alias
+            setTextColor(Color.BLACK)
+            textSize = 14f
+            setTypeface(typeface, android.graphics.Typeface.BOLD)
+            gravity = Gravity.START
+        }
+        messageLayout.addView(aliasText)
 
-        textView.text = message
-        textView.setPadding(16, 16, 16, 16)
-        textView.setBackgroundResource(
-            if (isMine) {
-                R.drawable.bocadillo_salida
-            } else {
-                R.drawable.bocadillo_entrada
-            })
-        textView.setTextColor(Color.BLACK)
-        return textView
+        val messageText = TextView(this).apply {
+            text = message
+            setTextColor(Color.BLACK)
+            textSize = 14f
+        }
+
+        val timestamp = TextView(this).apply {
+            text = obtenerHora() // Cambiar por hora real si está disponible
+            setTextColor(Color.GRAY)
+            textSize = 10f
+            gravity = Gravity.END
+        }
+
+        messageLayout.addView(messageText)
+        messageLayout.addView(timestamp)
+
+        return messageLayout
     }
 
-    private fun createImageView(bitmap: android.graphics.Bitmap): android.widget.ImageView {
-        val imageView = android.widget.ImageView(this)
-        imageView.setImageBitmap(bitmap)
-        return imageView
-    }
-
+    // Muestra mensaje mediante Toast
     private fun showToast(message: String) {
         runOnUiThread {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
     }
 
+    // Valida si una dirección IP es válida en formato IPv4
     fun esDireccionIPv4Valida(ip: String): Boolean {
         val partes = ip.split(".")
         if (partes.size != 4) return false
