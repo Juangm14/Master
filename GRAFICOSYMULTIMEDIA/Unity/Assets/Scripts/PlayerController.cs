9405c8e8d8c064db9a0285ef293f53e60c8c4101
Juan García Martínez <jgm179@alu.ua.es>
2025-02-27 22:23:46 +0100

    Incluyendo mapa 5 en unity

--- GRAFICOSYMULTIMEDIA/Unity/Assets/Scripts/PlayerController.cs
+++ GRAFICOSYMULTIMEDIA/Unity/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float velocidad;
     public Text countText;
     public Text winText;
@@ -12,42 +10,45 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private int contador;
 
+    public float fuerzaSalto = 5f; // Define la fuerza con la que el jugador saltará.
+    private bool enSuelo; // Variable para saber si el jugador está en el suelo.
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         contador = 0;
         SetCountText();
         winText.text = "";
-        rb.constraints = RigidbodyConstraints.FreezePositionY;
-
-        // Verifica que la gravedad esté activada
         rb.useGravity = true;
         rb.constraints = RigidbodyConstraints.None;
 
-        // Verifica que el Collider del jugador esté configurado correctamente
         Collider playerCollider = GetComponent<Collider>();
         if (playerCollider == null)
         {
-            // Si no tiene un collider, puedes agregarlo manualmente
             playerCollider = gameObject.AddComponent<SphereCollider>();
         }
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         float posH = Input.GetAxis("Horizontal");
         float posV = Input.GetAxis("Vertical");
 
-        if (transform.position.y < -10.0)
+        if (transform.position.y < -10.0f)
         {
             winText.text = "Perdiste!!";
             Invoke("QuitGame", 1.5f);
         }
 
         Vector3 movimiento = new Vector3(posH, 0.0f, posV);
-
         rb.AddForce(movimiento * velocidad);
+
+        // Detecta si se presiona la barra espaciadora y si el jugador está en el suelo.
+        if (Input.GetKeyDown(KeyCode.Space) && enSuelo) 
+        {
+            rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse); // Aplica una fuerza hacia arriba para realizar el salto.
+            enSuelo = false; // Evita que el jugador haga saltos dobles hasta que vuelva a tocar el suelo.
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -55,11 +56,15 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("mono"))
         {
             other.gameObject.SetActive(false);
-            contador = contador + 1;
+            contador++;
             SetCountText();
-
         }
+    }
 
+    // Detecta si el jugador toca el suelo.
+    void OnCollisionEnter(Collision collision) 
+    {
+        enSuelo = true;
     }
 
     void SetCountText()
@@ -75,12 +80,9 @@ public class PlayerController : MonoBehaviour
     void QuitGame()
     {
 #if UNITY_EDITOR
-		UnityEditor.EditorApplication.isPlaying = false;
-#elif UNITY_WEBPLAYER
-		Application.OpenURL(webplayerQuitURL);
+        UnityEditor.EditorApplication.isPlaying = false;
 #else
         Application.Quit();
 #endif
     }
-
 }
